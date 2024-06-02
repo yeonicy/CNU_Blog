c03db54e81dc4cc000ae64eb157d5f58126c6830
chaeyeon <yeonicy@naver.com>
2024-06-02 23:42:25 +0900

    프로젝트 내역수정

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -7,83 +7,83 @@ import useCreatePost from '../queries/useCreatePost';
 import useUpdatePostById from '../queries/useUpdatePostById';
 
 const TitleInput = styled.input`
-    display: block;
-    width: 100%;
-    height: 66px;
-    background: transparent;
-    padding: 2rem 0 0 0;
-    font-size: 2.75rem;
-    resize: none;
-    line-height: 1.5;
-    outline: none;
-    border: none;
-    font-weight: bold;
-    color: #212529;
+  display: block;
+  width: 100%;
+  height: 66px;
+  background: transparent;
+  padding: 2rem 0 0 0;
+  font-size: 2.75rem;
+  resize: none;
+  line-height: 1.5;
+  outline: none;
+  border: none;
+  font-weight: bold;
+  color: #212529;
 `;
 
 const TagSelect = styled.select`
-    font-size: 1.125rem;
-    line-height: 2rem;
-    margin-bottom: 0.75rem;
-    min-width: 8rem;
-    color: #212529;
-    border: none;
+  font-size: 1.125rem;
+  line-height: 2rem;
+  margin-bottom: 0.75rem;
+  min-width: 8rem;
+  color: #212529;
+  border: none;
 `;
 
 const Editor = styled.textarea`
-    width: 100%;
-    height: calc(100% - 200px);
-    min-height: 100px;
-    border: none;
-    resize: none;
-    font-size: 1.125rem;
-    flex: 1 1 0%;
+  width: 100%;
+  height: calc(100% - 200px);
+  min-height: 100px;
+  border: none;
+  resize: none;
+  font-size: 1.125rem;
+  flex: 1 1 0%;
 `;
 
 const BottomSheet = styled.div`
-    bottom: 0;
-    width: 760px;
-    height: 4rem;
-    background: #ffffff;
-    border-top: 1px solid #ddd;
-    display: flex;
-    -webkit-box-pack: justify;
-    justify-content: space-between;
-    -webkit-box-align: center;
-    align-items: center;
-    padding-left: 1rem;
-    padding-right: 1rem;
+  bottom: 0;
+  width: 760px;
+  height: 4rem;
+  background: #ffffff;
+  border-top: 1px solid #ddd;
+  display: flex;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  -webkit-box-align: center;
+  align-items: center;
+  padding-left: 1rem;
+  padding-right: 1rem;
 `;
 
 const ExitButton = styled.button`
-    height: 2.5rem;
-    padding: 0.5rem 1rem;
-    -webkit-box-align: center;
-    align-items: center;
-    background: none;
-    border-radius: 4px;
-    cursor: pointer;
-    border: none;
-    display: flex;
-    outline: none;
-    color: #212529;
-    font-size: 1.125rem;
+  height: 2.5rem;
+  padding: 0.5rem 1rem;
+  -webkit-box-align: center;
+  align-items: center;
+  background: none;
+  border-radius: 4px;
+  cursor: pointer;
+  border: none;
+  display: flex;
+  outline: none;
+  color: #212529;
+  font-size: 1.125rem;
 `;
 
 const SaveButton = styled.button`
-    height: 2rem;
-    padding-left: 1rem;
-    padding-right: 1rem;
-    font-size: 1rem;
-    border-radius: 4px;
-    outline: none;
-    font-weight: bold;
-    word-break: keep-all;
-    background: rgb(50, 148, 248);
-    border: 1px solid rgb(50, 148, 248);
-    color: #ffffff;
-    transition: all 0.125s ease-in 0s;
-    cursor: pointer;
+  height: 2rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  font-size: 1rem;
+  border-radius: 4px;
+  outline: none;
+  font-weight: bold;
+  word-break: keep-all;
+  background: rgb(50, 148, 248);
+  border: 1px solid rgb(50, 148, 248);
+  color: #ffffff;
+  transition: all 0.125s ease-in 0s;
+  cursor: pointer;
 `;
 
 const Write = () => {
