5d5154f6c9e55c50675f2024e2b7dbaebb9e85ac
chaeyeon <yeonicy@naver.com>
2024-06-03 00:01:45 +0900

    변경사항 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
     <div>
       <Wrapper>
         <Link to="/">
-          <h2>dohyeon.log</h2>
+          <h2>chaeyeon.log</h2>
         </Link>
         <Link to="/write">
           <Button className="write-button">글쓰기</Button>
